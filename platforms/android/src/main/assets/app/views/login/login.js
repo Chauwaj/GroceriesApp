cb554eb5d6b5f5ce9110b67ac3f99368b93b3501
Jonathan Chauwa <chauwaj@berea.edu>
2017-07-09 00:07:09 -0400

    Second commit

--- platforms/android/src/main/assets/app/views/login/login.js
+++ platforms/android/src/main/assets/app/views/login/login.js
@@ -1,9 +1,14 @@
-var observableModule = require("data/observable");
+var dialogsModule = require("ui/dialogs");
 
-var user = new observableModule.fromObject({
-    email: "user@domain.com",
-    password: "password"
-});
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var user = new UserViewModel();
+
+// var observableModule = require("data/observable");
+
+// var user = new observableModule.fromObject({
+//     email: "user@domain.com",
+//     password: "password"
+// });
 
 var frameModule = require("ui/frame");
 var page;
@@ -14,9 +19,24 @@ exports.loaded = function(args) {
     page.bindingContext = user;
 };
 exports.signIn = function() {
-    email = page.getViewById("email");
-    console.log(email.text);
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
 };
+// exports.signIn = function() {
+//     user.login();
+//     // email = page.getViewById("email");
+//     // console.log(email.text);
+// };
 
 exports.register = function() {
     var topmost = frameModule.topmost();
